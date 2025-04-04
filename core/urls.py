772e2710d4d05e8edaac6fa8731c3c5a2cec8154
aliazizi-code <devaliazizi@gmail.com>
2025-04-04 10:54:11 +0330

    Add course URLs and configure media serving in development

--- core/urls.py
+++ core/urls.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path, include
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
@@ -6,9 +8,14 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls')),
     path('blog/', include('blog.urls')),
+    path('courses/', include('courses.urls')),
 
     # document schema patterns
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
     # document schema optional ui
     path('api/schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
 ]
+
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
