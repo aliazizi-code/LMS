b1b2180d5eb02e474b416a2f45463694958a016b
aliazizi-code <devaliazizi@gmail.com>
2025-04-04 18:43:07 +0330

    Enhance UserAdmin in accounts/admin.py with custom list display, filtering, and search fields

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from . import models
+
+
+class UserAdmin(UserAdmin):
+    list_display = ('phone', 'email', 'first_name', 'last_name', 'is_staff', 'is_active')
+    list_filter = ('is_staff', 'is_active')
+    search_fields = ('phone', 'email', 'first_name', 'last_name')
+    ordering = ('phone',)
+
+    fieldsets = (
+        (None, {'fields': ('phone', 'email', 'first_name', 'last_name', 'is_active', 'is_staff')}),
+    )
+
+
+admin.site.register(models.User, UserAdmin)
+admin.site.register(models.UserProfile)
