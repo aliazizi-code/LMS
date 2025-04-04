37ac81e723e44df894cfe1943fe1f211e84f4a61
aliazizi-code <devaliazizi@gmail.com>
2025-04-04 18:30:42 +0330

    Refactor Course and Category views to integrate Django Filter Backend and update pagination settings

--- courses/views.py
+++ courses/views.py
@@ -1,20 +1,14 @@
-from rest_framework import viewsets, generics, status
-from rest_framework.views import APIView
-from rest_framework.response import Response
+from rest_framework import viewsets, generics
 from rest_framework.pagination import PageNumberPagination
+from django_filters.rest_framework import DjangoFilterBackend
 
 from courses.models import Course
 from courses.serializers import *
+from .filters import CourseFilter
 
 
 class CourseListPagination(PageNumberPagination):
     page_size = 16
-    page_size_query_param = 'page_size'
-    max_page_size = 100
-    
-
-class CategoryListPagination(PageNumberPagination):
-    page_size = 20
     page_size_query_param = 'page_size'
     max_page_size = 100
 
@@ -27,8 +21,8 @@ class CourseListView(viewsets.ModelViewSet):
     ).distinct()
     serializer_class = CourseListSerializer
     pagination_class = CourseListPagination
-    # filter_backends = (DjangoFilterBackend,)
-    # filterset_class = EventFilter
+    filter_backends = (DjangoFilterBackend,)
+    filterset_class = CourseFilter
     
 
 class CourseDetailView(generics.RetrieveAPIView):
@@ -42,13 +36,6 @@ class CourseDetailView(generics.RetrieveAPIView):
 
 
 class CategoryListView(generics.ListAPIView):
-    serializer_class = CategoryListSerializer
-    pagination_class = CategoryListPagination
-    lookup_field = 'slug'
-    
-    def get_queryset(self):
-        slug = self.kwargs.get(self.lookup_field)
-        if slug:
-            return CourseCategory.objects.filter(parent__slug=slug, is_active=True)
-        return CourseCategory.objects.filter(parent=None, is_active=True)
+    serializer_class = CategoryWithChildrenSerializer
+    queryset = CourseCategory.objects.filter(parent=None, is_active=True).distinct()
             
