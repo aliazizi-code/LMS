32504b560f37d3d0ff8d903c572a05fe0c49c1cc
Farzad Nari <farzadnari10@gmail.com>
2025-04-04 17:21:38 +0330

    define ArticleCategory model

--- blog/models.py
+++ blog/models.py
@@ -1,2 +1,14 @@
 from django.db import models
 from mptt.models import MPTTModel, TreeForeignKey
+from autoslug import AutoSlugField
+
+
+class ArticleCategory(MPTTModel):
+    name = models.CharField(max_length=100)
+    slug = AutoSlugField(populate_from=name, unique=True)
+    description = models.TextField()
+    parent = TreeForeignKey('self', on_delete=models.CASCADE, null=True, blank=True, related_name='children')
+    is_active = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
