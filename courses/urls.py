74a0853b758ecef61f349232611cfa02ea6b4a16
aliazizi-code <devaliazizi@gmail.com>
2025-04-04 18:30:54 +0330

    Remove unused URL pattern for category children in courses/urls.py

--- courses/urls.py
+++ courses/urls.py
@@ -5,6 +5,5 @@ from courses.views import *
 urlpatterns = [
     path('', CourseListView.as_view({'get': 'list'}), name='course-list'),
     path('categories/', CategoryListView.as_view(), name='categories-list'),
-    re_path(r'^categories/(?P<slug>[\w\-آ-ی]+)/?$', CategoryListView.as_view(), name='category_children'),
     re_path(r'^(?P<slug>[\w\-آ-ی]+)/?$', CourseDetailView.as_view(), name='course-detail')
 ]
