b9dc187a1d945ae66c9a9f5252fe0d3f1e7a6403
Farzad Nari <farzadnari10@gmail.com>
2025-04-06 02:53:27 +0330

    create slug_field file in utils

---
+++ utils/slug_field.py
@@ -0,0 +1,22 @@
+import re
+from django.db import models
+
+def slugify(value):
+    cleaned_value = re.sub(r'[^\w\s-]', '', str(value))
+    return re.sub(r'[-\s]+', '-', cleaned_value).lower().strip('-')
+
+class AutoSlugField(models.SlugField):
+    def __init__(self, source_field=None, *args, **kwargs):
+        self.source_field = source_field
+        kwargs.setdefault('max_length', 255)
+        kwargs.setdefault('unique', True)
+        kwargs.setdefault('db_index', True)
+        kwargs.setdefault('editable', False)
+        super().__init__(*args, **kwargs)
+
+    def pre_save(self, model_instance, add):
+        if self.source_field:
+            value = getattr(model_instance, self.source_field)
+            slug = slugify(value)
+            setattr(model_instance, self.attname, slug)
+        return super().pre_save(model_instance, add)
