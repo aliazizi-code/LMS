cc09c4d852596a219f46cef24894c08679d53670
Farzad Nari <farzadnari10@gmail.com>
2025-04-06 02:48:04 +0330

    define Article model  in blog.models

--- blog/models.py
+++ blog/models.py
@@ -2,9 +2,17 @@ from django.db import models
 from mptt.models import MPTTModel, TreeForeignKey
 from autoslug import AutoSlugField
 from django.utils.translation.trans_null import gettext_lazy as _
-from accounts.models import User
 from markdownfield.models import MarkdownField, RenderedMarkdownField
 from markdownfield.validators import VALIDATOR_STANDARD
+from taggit.managers import TaggableManager
+from imagekit.models import ImageSpecField
+from imagekit.processors import ResizeToFill
+from django.conf import settings
+from utils import get_upload_to, validate_image_size
+
+
+def get_upload_image(instance, filename):
+    return get_upload_to(instance, filename, prefix='Article/image')
 
 
 class ArticleCategory(MPTTModel):
@@ -20,7 +28,7 @@ class ArticleCategory(MPTTModel):
         verbose_name = _('Article Category')
         verbose_name_plural = _('Article Categories')
         ordering = ['name']
-        db_table = 'article_categories'
+        db_table = 'article_category'
 
     class MPTTMeta:
         order_insertion_by = ['name']
@@ -29,3 +37,44 @@ class ArticleCategory(MPTTModel):
         return self.name
     
 
+class Article(models.Model):
+    class STATUS(models.TextChoices):
+        IN_REVIEW = '-در-حال-بررسی', _('در-حال-بررسی')
+        PUBLISHED = 'منتشر-شده-است', _('منتشر-شده-است')
+        NEEDS_CORRECTION = 'نیاز-به-اصلاح دارد', _('نیاز-به-اصلاح دارد')
+        NOT_CONFIRMED = 'تایید-نشد', _('تایید-نشد')
+
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='articles')
+    title = models.CharField(max_length=250)
+    slug = AutoSlugField(populate_from=title, unique=True)
+    image = models.ImageField(
+        upload_to=get_upload_image,
+        validators=validate_image_size,
+
+    )
+    # image_thumbnail need to fix
+    image_thumbnail = ImageSpecField(
+        source='image',
+        processors=[ResizeToFill(120, 120)],
+        format='jpg',
+        options={'quality': 80}
+    ) 
+    category = models.ManyToManyField(ArticleCategory, related_name="articles")
+    content = MarkdownField(rendered_field='content_rendered', validator=VALIDATOR_STANDARD)
+    content_rendered = RenderedMarkdownField() 
+    short_description = models.TextField()
+    tags = TaggableManager()
+    status = models.CharField(
+        max_length=30,
+        choices=STATUS.choices,
+        default=STATUS.IN_REVIEW,
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    published_at = models.DateField(null=True, blank=True)
+
+    class Meta:
+        verbose_name = _('Article')
+        verbose_name_plural = _('Articles')
+        ordering = ['-created_at']
+        db_table = 'article'
