71f50843ba562344f55e69c678a3eca116247012
aliazizi-code <devaliazizi@gmail.com>
2025-04-07 18:33:14 +0330

    Refactor urls.py to organize teacher-related routes; add paths for creating and listing courses by teacher, managing seasons, and retrieving course details.

--- courses/urls.py
+++ courses/urls.py
@@ -2,8 +2,47 @@ from django.urls import path, re_path
 
 from courses.views import *
 
+
+course_teacher_methods = {
+    
+    
+}
+
+
 urlpatterns = [
     path('', CourseListView.as_view({'get': 'list'}), name='course-list'),
+    
+    path('learning-level/', LearningLevelView.as_view(), name='learning-level'),
+    
+    
+    path(
+        'teacher/create/',
+        CourseByTeacherViewSet.as_view({'post': 'create'}),
+        name='course-teacher-create'
+    ),
+    path(
+        'teacher/list/',
+        CourseByTeacherListView.as_view(),
+        name='course-teacher-list'
+    ),
+    path(
+        'teacher/season/',
+        SeasonView.as_view({'get': 'list', 'post': 'create'}),
+        name='course-teacher-season'
+    ),
+    path(
+        'teacher/season/<int:pk>/',
+        SeasonView.as_view({'get': 'retrieve', 'patch': 'partial_update', 'delete': 'destroy'}),
+        name='course-teacher-season-detail'
+    ),
+    re_path(
+        r'^teacher/(?P<slug>[\w\-آ-ی]+)/?$',
+        CourseByTeacherViewSet.as_view({'get': 'retrieve', 'patch': 'partial_update', 'delete': 'destroy'}),
+        name='course-teacher-detail'
+    ),
+    
+    
     path('categories/', CategoryListView.as_view(), name='categories-list'),
-    re_path(r'^(?P<slug>[\w\-آ-ی]+)/?$', CourseDetailView.as_view(), name='course-detail')
+    
+    re_path(r'^(?P<slug>[\w\-آ-ی]+)/?$', CourseDetailView.as_view(), name='course-detail'),
 ]
