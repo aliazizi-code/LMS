b3d1b1fab9b85adebb6790f39e357d087e9d83d6
aliazizi-code <devaliazizi@gmail.com>
2025-04-07 18:32:51 +0330

    Refactor signals.py to enhance Course signal handlers; add price creation for new courses and update search vector for course instances.

--- courses/signals.py
+++ courses/signals.py
@@ -1,9 +1,25 @@
 from django.db.models.signals import post_save
+from django.contrib.postgres.search import SearchVector
 from django.dispatch import receiver
-from courses.models import CourseCategory
+from courses.models import CourseCategory, Course, Price
 
 
 @receiver(post_save, sender=CourseCategory)
 def update_active_status_of_descendants(sender, instance, **kwargs):
     descendants = instance.get_descendants(include_self=True)
     descendants.update(is_active=instance.is_active)
+
+
+@receiver(post_save, sender=Course)
+def create_price_for_course(sender, instance, created, **kwargs):
+    if created:
+        Price.objects.create(course=instance, main_price=0, final_price=0)
+
+
+@receiver(post_save, sender=Course)
+def update_search_vector(sender, instance, **kwargs):
+    Course.objects.filter(
+        id=instance.id
+    ).update(
+        sv=SearchVector('title', 'short_description')
+    )
