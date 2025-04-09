e6c2cfdcf35230cdc5b65da921a2e347b0689e8d
aliazizi-code <devaliazizi@gmail.com>
2025-04-09 13:57:55 +0330

    Refactor IsTeacher permission to include user authentication and activity checks

--- courses/permissions.py
+++ courses/permissions.py
@@ -1,6 +1,15 @@
 from rest_framework.permissions import BasePermission
-
+from rest_framework.exceptions import PermissionDenied
 
 class IsTeacher(BasePermission):
     def has_permission(self, request, view):
-        return request.user.has_perm('courses.can_teacher')
+        if not request.user.is_authenticated:
+            return False
+
+        if not request.user.is_active:
+            raise PermissionDenied('کاربر فعال نیست.')
+
+        if not (request.user.has_perm('courses.can_teacher') and request.user.has_perm('accounts.can_employee')):
+            raise PermissionDenied('کاربر مجوزهای لازم را ندارد.')
+
+        return True
