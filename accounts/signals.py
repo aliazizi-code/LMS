9f82e11958b3fe01653070282885a1aa2016643b
aliazizi-code <devaliazizi@gmail.com>
2025-04-10 13:12:20 +0330

    Refactor permission creation in post_migrate signal and add CustomGroup creation

--- accounts/signals.py
+++ accounts/signals.py
@@ -2,7 +2,7 @@ from django.db.models.signals import post_save, post_migrate
 from django.contrib.auth.models import Permission, Group
 from django.contrib.contenttypes.models import ContentType
 from django.dispatch import receiver
-from accounts.models import JobCategory, EmployeeProfile
+from accounts.models import JobCategory, EmployeeProfile, CustomGroup
 from courses.models import Course
 # from blog.models import Article # (uncomment if needed)
 
@@ -12,31 +12,6 @@ def update_active_status_of_descendants(sender, instance, **kwargs):
     descendants = instance.get_descendants(include_self=True)
     descendants.update(is_active=instance.is_active)
 
-
-@receiver(post_migrate)
-def teacher_permission(sender, **kwargs):
-    if sender.name == 'accounts':
-        content_type = ContentType.objects.get_for_model(Course)
-        permission, _ = Permission.objects.get_or_create(
-            codename='can_teacher',
-            name='Can teacher',
-            content_type=content_type,
-        )
-        event_creators_group, _ = Group.objects.get_or_create(name='Teacher')
-        event_creators_group.permissions.add(permission)
-
-
-@receiver(post_migrate)
-def author_permission(sender, **kwargs):
-    if sender.name == 'accounts':
-        content_type = ContentType.objects.get_for_model(Course)
-        permission, _ = Permission.objects.get_or_create(
-            codename='can_author',
-            name='Can author',
-            content_type=content_type,
-        )
-        event_creators_group, _ = Group.objects.get_or_create(name='Author')
-        event_creators_group.permissions.add(permission)
 
 @receiver(post_migrate)
 def create_permissions(sender, **kwargs):
@@ -49,7 +24,7 @@ def create_permissions(sender, **kwargs):
             name='Can teacher',
             content_type=content_type_course,
         )
-        teacher_group, _ = Group.objects.get_or_create(name='Teacher')
+        teacher_group, _ = Group.objects.get_or_create(name='مدرسین')
         teacher_group.permissions.add(teacher_permission)
 
         # Create Employee Permission
@@ -59,7 +34,7 @@ def create_permissions(sender, **kwargs):
             name='Can employee',
             content_type=content_type_employee,
         )
-        employee_group, _ = Group.objects.get_or_create(name='Employee')
+        employee_group, _ = Group.objects.get_or_create(name='کارمندان')
         employee_group.permissions.add(employee_permission)
         
         # Create Author Permission (uncomment if needed)
@@ -69,5 +44,11 @@ def create_permissions(sender, **kwargs):
         #     name='Can author',
         #     content_type=content_type_blog,
         # )
-        # author_group, _ = Group.objects.get_or_create(name='Author')
+        # author_group, _ = Group.objects.get_or_create(name='نویسندگان')
         # author_group.permissions.add(author_permission)
+
+
+@receiver(post_save, sender=Group)
+def create_custom_group(sender, instance, created, **kwargs):
+    if created:
+        CustomGroup.objects.create(group=instance)
