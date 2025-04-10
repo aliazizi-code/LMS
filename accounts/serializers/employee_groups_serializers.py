c82cfc9ce401ec0bfcddbc8b5593a4b62accc5bc
aliazizi-code <devaliazizi@gmail.com>
2025-04-10 17:17:58 +0330

    Refactor GroupSerializer and add EmployeeDetailSerializer for improved user profile handling

--- accounts/serializers/employee_groups_serializers.py
+++ accounts/serializers/employee_groups_serializers.py
@@ -3,13 +3,6 @@ from accounts.models import UserProfile
 from django.contrib.auth.models import Group
 
 
-class UserProfileSerializer(serializers.ModelSerializer):
-    full_name = serializers.CharField(source='user.full_name')
-    class Meta:
-        model = UserProfile
-        fields = ['full_name', 'avatar']
-
-
 class GroupSerializer(serializers.ModelSerializer):
     members = serializers.SerializerMethodField()
 
@@ -18,19 +11,68 @@ class GroupSerializer(serializers.ModelSerializer):
         fields = ['name', 'members']
 
     def get_members(self, obj):
-        members = []
-        for user in obj.user_set.all():
-            try:
-                user_profile = UserProfile.objects.get(user=user)
-                employee_profile = user_profile.employee_profile
-                members.append({
-                    "full_name": user_profile.user.full_name(),
-                    "avatar_thumbnail": user_profile.avatar_thumbnail.url if user_profile.avatar else None,
-                    "username": employee_profile.username,
-                })
-            except UserProfile.DoesNotExist:
-                members.append({
-                    "full_name": user.full_name(),
-                    "avatar_thumbnail": None
-                })
-        return members
+        user_profiles = UserProfile.objects.filter(
+            user__groups=obj,
+            user__first_name__isnull=False,
+            user__last_name__isnull=False,
+            bio__isnull=False,
+            job__isnull=False,
+            avatar__isnull=False,
+            age__isnull=False,
+            gender__isnull=False,
+            employee_profile__username__isnull=False,
+            employee_profile__social_link__isnull=False,
+        ).select_related(
+            'employee_profile', 'user'
+        ).only(
+            'avatar', 
+            'user__first_name', 
+            'user__last_name',
+            'employee_profile__username'
+        )
+        
+        return [
+            {
+                "full_name": f"{profile.user.first_name} {profile.user.last_name}",
+                "username": profile.employee_profile.username,
+                "avatar_thumbnail": profile.avatar_thumbnail.url,
+            }
+            for profile in user_profiles
+        ]
+
+
+class EmployeeDetailSerializer(serializers.ModelSerializer):
+    groups = serializers.SerializerMethodField()
+    full_name = serializers.CharField(source='user.full_name')
+    position = serializers.CharField(source='employee_profile.position')
+    skills = serializers.SerializerMethodField()
+    social_links = serializers.SerializerMethodField()
+    
+
+    class Meta:
+        model = UserProfile
+        fields = (
+            'full_name', 'avatar', 'bio',
+            'job', 'gender', 'age', 'position',
+            'skills', 'social_links', 'groups', 
+        )
+        
+    def get_groups(self, obj):
+        group_names = obj.user.groups.filter(
+            custom_groups__is_display=True
+        ).values_list('name', flat=True)
+        return list(group_names)
+    
+    def get_skills(self, obj):
+        skills = obj.employee_profile.skills.all().values_list('name', flat=True)
+        return list(skills)
+    
+    def get_social_links(self, obj):
+        social_links = obj.employee_profile.social_link.all()
+        return [
+            {
+                "link": social_link.link,
+                "type": social_link.social_media_type,
+            }
+            for social_link in social_links
+        ]
