aa22fc136cab8b912f03e1ec5afb9f4ebe626e99
aliazizi-code <devaliazizi@gmail.com>
2025-04-10 17:17:02 +0330

    Remove unused slugged_filename variable from get_upload_to function

--- utils/get_upload_to_for_image.py
+++ utils/get_upload_to_for_image.py
@@ -1,6 +1,5 @@
 from utils.custom_fields.slug_field import slugify
 
 def get_upload_to(instance, filename, prefix):
-        slugged_filename = slugify(filename)
         slugged_instance = slugify(instance)
-        return f'{prefix}/{slugged_instance}/{slugged_filename}'
+        return f'{prefix}/{slugged_instance}'
