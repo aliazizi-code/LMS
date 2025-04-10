74ccf2b001b0a30ef5f617584e915fc74cef61c5
aliazizi-code <devaliazizi@gmail.com>
2025-04-10 13:13:14 +0330

    Refactor user profile and employee endpoints for improved readability and organization

--- accounts/urls.py
+++ accounts/urls.py
@@ -12,9 +12,44 @@ urlpatterns = [
     path('auth/token/refresh/', views.RefreshTokenAPIView.as_view(), name='token_refresh'),
 
     # User profile management
-    path('user/profile/', views.UserProfileViewSet.as_view({'get': 'retrieve', 'patch': 'partial_update'}), name='user-profile'),
-    path('user/phone/change/request/', views.ChangePhoneRequestView.as_view(), name='change-phone-request'),
+    path(
+        'user/profile/',
+        views.UserProfileViewSet.as_view(
+        {
+            'get': 'retrieve',
+            'patch': 'partial_update',
+        }),
+        name='user-profile'
+    ),
+    path(
+        'user/phone/change/request/',
+        views.ChangePhoneRequestView.as_view(),
+        name='change-phone-request'
+    ),
+    path(
+        'user/employee/profile/',
+        views.EmployeeProfileViewSet.as_view(
+            {
+                'get': 'retrieve',
+                'patch': 'partial_update',
+                'post': 'create',
+            }),
+        name='employee-profile'
+    ),
+    path(
+        'user/employee/social-link/',
+        views.EmployeeSocialLinkViewSet.as_view(
+            {
+                'get': 'retrieve',
+                'patch': 'partial_update',
+                'post': 'create',
+            }),
+        name='employee-social-link'
+    ),
+    path('employee/<str:username>/', views.EmployeeDetailView.as_view(), name='employee-detail'),
+    path('user/employee/social-links/', views.EmployeeSocialLinkViewSet.as_view({'get': 'list'}), name='employee-social-links-list'),
     path('user/phone/change/verify/', views.ChangePhoneVerifyView.as_view(), name='change-phone-verify'),
+    path('groups/', views.GroupListView.as_view(), name='group-list'),
 
     # Password management
     path('password/reset/', views.ForgotPasswordView.as_view(), name='forgot-password-request'),
