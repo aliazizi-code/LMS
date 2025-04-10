8f3f24eee69b7c3be7cae50d103ce700143ae539
aliazizi-code <devaliazizi@gmail.com>
2025-04-10 13:11:51 +0330

    Refactor import statements for better organization and clarity

--- utils/__init__.py
+++ utils/__init__.py
@@ -2,15 +2,15 @@ from .cache_manager import *
 
 from .otp import *
 
-from .validators_image import *
+from .validate.validators_image import *
+from.validate .validators_persian import *
 
-from .email_token import *
+from .email_token_manager import *
 
 from .get_upload_to_for_image import *
 
 from .slug_field import *
 from .get_discounted_price import *
 
-from .slug_field import *
-
-from .phone_number_field import *
+from custom_fields.phone_number_field import *
+from custom_fields.slug_field import *
