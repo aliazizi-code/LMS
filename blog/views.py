2ef595f58b68d0704dd098fdf47476cf6792a990
Farzad Nari <farzadnari10@gmail.com>
2025-04-11 17:25:21 +0330

    define ArticleViewSet

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,8 @@
 from rest_framework import generics
-from .serializers import ArticleCategorySerializer
+from .serializers import ArticleCategorySerializer, ArticleSerializer
 from .models import *
+from rest_framework.viewsets import ModelViewSet
+from rest_framework.permissions import IsAuthenticated
 
 
 class CategoryListView(generics.ListAPIView):
@@ -8,6 +10,12 @@ class CategoryListView(generics.ListAPIView):
     queryset = ArticleCategory.objects.filter(parent=None, is_active=True)
 
 
+class ArticleViewSet(ModelViewSet):
+    queryset = Article.objects.all()
+    serializer_class = ArticleSerializer
+    permission_classes = [IsAuthenticated]
+
+    
 
 
 
