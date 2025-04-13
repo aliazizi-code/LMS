4dbf12e667d93d77018af15a9ac3f4833aca9f97
aliazizi-code <devaliazizi@gmail.com>
2025-04-13 13:25:10 +0330

    Refactor TeamListView to use EmployeeProfile instead of Group; streamline queryset retrieval for employee profiles

--- accounts/views/employee_groups_views.py
+++ accounts/views/employee_groups_views.py
@@ -1,21 +1,20 @@
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.views import APIView
-from django.contrib.auth.models import Group
-from django.shortcuts import get_object_or_404
 
-from accounts.models import EmployeeProfile, SocialLink, UserProfile
+from accounts.models import EmployeeProfile
 from accounts.serializers import (
-    GroupSerializer,
+    TeamSerializer,
     EmployeeDetailSerializer,
 )
 
 
-class GroupListView(APIView):
+class TeamListView(APIView):
+    serializer_class = TeamSerializer
 
     def get(self, request):
-        queryset = Group.objects.filter(custom_groups__is_display=True)
-        serializer = GroupSerializer(queryset, many=True)
+        queryset = EmployeeProfile.objects.filter_completed_profiles()
+        serializer = self.serializer_class(queryset, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
 
 
@@ -23,23 +22,11 @@ class EmployeeDetailView(APIView):
     serializer_class = EmployeeDetailSerializer
 
     def get(self, request, username=None):
-        queryset = get_object_or_404(
-            UserProfile,
-            employee_profile__username=username,
-            gender__isnull=False,
-            age__isnull=False,
-            bio__isnull=False,
-            job__isnull=False,
-            avatar__isnull=False,
-            user__first_name__isnull=False,
-            user__last_name__isnull=False,
-        )
+        queryset = EmployeeProfile.objects.filter_completed_profiles().filter(username=username).first()
+        
+        if queryset is None:
+            return Response({"خطا": "پروفایل کارمندی یافت نشد"}, status=status.HTTP_404_NOT_FOUND)
+        
         serializer = self.serializer_class(queryset)
-        
-        has_social_link = bool(queryset.employee_profile.social_link.all())
-        has_groups = bool(queryset.user.groups.filter(custom_groups__is_display=True))
-        
-        if not has_social_link or not has_groups:
-            return Response({},status=status.HTTP_400_BAD_REQUEST)
         
         return Response(serializer.data, status=status.HTTP_200_OK)
