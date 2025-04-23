4e5b9e2292034875731d3703081d0cd28d8e8e6e
aliazizi-code <devaliazizi@gmail.com>
2025-04-23 23:18:11 +0330

    Refactor course models and upload functions: enhance file upload handling, add request choices, and implement CourseRequest model with validation.

--- courses/models.py
+++ courses/models.py
@@ -4,18 +4,80 @@ from django.core.validators import MaxValueValidator, MinValueValidator, MaxLeng
 from django.db import models
 from django.utils import timezone
 from django.utils.translation import gettext_lazy as _
-from imagekit.models import ImageSpecField
-from taggit.managers import TaggableManager
 from django.contrib.postgres.search import SearchVectorField
 from django.contrib.postgres.indexes import GinIndex
+from simple_history.models import HistoricalRecords
+from imagekit.models import ImageSpecField
+from taggit.managers import TaggableManager
 from mptt.models import MPTTModel, TreeForeignKey
 
 from utils import get_upload_to, validate_image_size, AutoSlugField
 
 
-def get_upload_path(instance, filename):
-    return get_upload_to(instance, filename, prefix='Course/banner')
+# region Upload Patch
 
+def get_upload_banner(instance, filename):
+    model_name = 'Course'
+    object_name = f"{instance.slug}-{instance.id}"
+    folder_type = 'banner'
+    return get_upload_to(instance, filename, model_name, object_name, folder_type)
+
+
+def get_upload_videos(instance, filename):
+    model_name = 'Course'
+    object_name = f"{instance.course.slug}-{instance.course.id}"
+    folder_type = 'videos'
+    return get_upload_to(instance, filename, model_name, object_name, folder_type)
+
+
+def get_upload_attachments(instance, filename):
+    model_name = 'Course'
+    object_name = f"{instance.course.slug}-{instance.course.id}"
+    folder_type = 'attachments'
+    return get_upload_to(instance, filename, model_name, object_name, folder_type)
+
+# endregion
+
+
+# region Choice class
+
+class RequestActionChoices(models.TextChoices):
+        ADD = 'add', _('ایجاد')
+        UPDATE = 'update', _('ویرایش')
+        DELETE = 'delete', _('حذف')
+
+
+class RequestTargetTypeChoices(models.TextChoices):
+    LESSON = 'lesson', _('درس')
+    FEATURE = 'feature', _('ویژگی')
+    FAQ = 'faq', _('سوالات متداول')
+    SEASON = 'season', _('فصل')
+    COURSE = 'course', _('دوره')
+
+
+class RequestStatusChoices(models.TextChoices):
+    PENDING = 'pending', _('در حال بررسی')
+    APPROVED = 'approved', _('تایید شده')
+    REJECTED = 'rejected', _('رد شده')
+    NEED_REVISION = 'need_revision', _('نیاز به اصلاح')
+    DRAFT = 'draft', _('پیش نویس')
+
+
+class CourseStatusChoices(models.TextChoices):
+    COMPLETED = 'COMPLETED', _('تکمیل شده')
+    IN_PROGRESS = 'IN_PROGRESS', _('در حال برگذاری')
+    UPCOMING = 'UPCOMING', _('به زودی')
+    CANCELLED = 'CANCELLED', _('لغو شده')
+    
+class CourseLanguageChoices(models.TextChoices):
+    # FA = 'fa', _('فارسی')
+    # EN = 'en', _('انگلیسی')
+    AZ = 'az', _('ترکی آذربایجانی')
+
+#endregion
+
+
+# region Model
 
 class LearningLevel(models.Model):
     name = models.CharField(max_length=100, unique=True, verbose_name=_('نام سطح آموزشی'))
@@ -37,7 +99,7 @@ class LearningLevel(models.Model):
 
 class LearningPath(models.Model):
     start_level = models.ForeignKey(
-        LearningLevel, related_name='learning_paths',
+        LearningLevel, related_name='learning_paths_start',
         on_delete=models.CASCADE, verbose_name=_('شروع سطح آموزشی')
     )
     end_level = models.ForeignKey(
@@ -111,21 +173,9 @@ class CourseCategory(MPTTModel):
 
 
 class Course(models.Model):
-    class STATUS(models.TextChoices):
-        COMPLETED = 'COMPLETED', _('تکمیل شده')
-        IN_PROGRESS = 'IN_PROGRESS', _('در حال برگذاری')
-        UPCOMING = 'UPCOMING', _('به زودی')
-        CANCELLED = 'CANCELLED', _('لغو شده')
-    
-    class LANGUAGE(models.TextChoices):
-        FA = 'fa', _('فارسی')
-        EN = 'en', _('انگلیسی')
-        AZ = 'az', _('ترکی آذربایجانی')
-
-    # region course fields
     title = models.CharField(max_length=200, verbose_name=_('عنوان دوره'))
     slug = AutoSlugField(source_field='title',verbose_name=_('آدرس دوره'))
-    sv = SearchVectorField(null=True, editable=False)
+    sv = SearchVectorField(blank=True, null=True, editable=False)
     description = models.TextField(verbose_name=_('توضیحات'))
     short_description = models.TextField(verbose_name=_('توضیحات کوتاه'))
     categories = models.ManyToManyField(
@@ -135,7 +185,7 @@ class Course(models.Model):
     tags = TaggableManager(verbose_name=_('برچسب ها'))
     language = models.CharField(
         max_length=50,
-        choices=LANGUAGE.choices,
+        choices=CourseLanguageChoices.choices,
         verbose_name=_("زبان دوره")
     )
     prerequisites = models.CharField(
@@ -149,7 +199,7 @@ class Course(models.Model):
         verbose_name=_('سطح مسیر آموزشی')
     )
     banner = models.ImageField(
-        upload_to=get_upload_path,
+        upload_to=get_upload_banner,
         validators=[validate_image_size],
         blank=True, null=True,
         verbose_name=_('بنر دوره')
@@ -169,7 +219,7 @@ class Course(models.Model):
     )
     status = models.CharField(
         max_length=30,
-        choices=STATUS.choices,
+        choices=CourseStatusChoices.choices,
         verbose_name=_('وضعیت دوره')
     )
     teacher = models.ForeignKey(
@@ -193,7 +243,6 @@ class Course(models.Model):
     created_at = models.DateTimeField(auto_now_add=True, editable=False, verbose_name=_('تاریخ ایجاد'))
     updated_at = models.DateTimeField(auto_now=True, editable=False, verbose_name=_('تاریخ بروزرسانی'))
     published_at = models.DateTimeField(blank=True, null=True, editable=False, verbose_name=_('تاریخ انتشار'))
-    # endregion
     
     def update_lesson_date(self):
         self.last_lesson_update = timezone.now()
@@ -204,9 +253,6 @@ class Course(models.Model):
         
         if self.is_published and self.pk:
             old_instance = Course.objects.get(pk=self.pk)
-            
-            if old_instance.is_deleted == False and self.is_deleted == True:
-                errors['is_deleted'] = _("امکان حذف دوره‌های منتشر شده وجود ندارد.")
                 
             TERMINAL_STATUSES = ['IN_PROGRESS', 'COMPLETED', 'CANCELLED']
             if old_instance.status in TERMINAL_STATUSES and self.status == 'UPCOMING':
@@ -243,23 +289,6 @@ class Feature(models.Model):
     created_at = models.DateTimeField(auto_now_add=True, verbose_name=_('تاریخ ایجاد'))
     updated_at = models.DateTimeField(auto_now=True, verbose_name=_('تاریخ بروزرسانی'))
     
-    def clean(self):
-        super().clean()
-        errors = {}
-        
-        if self.course.is_published and self.pk:
-            old_instance = Feature.objects.get(pk=self.pk)
-            
-            if old_instance.is_deleted == False and self.is_deleted == True:
-                errors['is_deleted'] = _("امکان حذف ویژگی برای دوره‌های منتشر شده وجود ندارد.")
-        
-        if errors:
-            raise ValidationError(errors)
-    
-    def save(self, *args, **kwargs):
-        self.clean()
-        super().save(*args, **kwargs)
-    
     class Meta:
         ordering = ['order', 'created_at', 'id']
         verbose_name = _('ویژگی')
@@ -277,23 +306,6 @@ class FAQ(models.Model):
     is_deleted = models.BooleanField(default=False, verbose_name=_('وضعیت حذف'))
     created_at = models.DateTimeField(auto_now_add=True, verbose_name=_('تاریخ ایجاد'))
     updated_at = models.DateTimeField(auto_now=True, verbose_name=_('تاریخ بروزرسانی'))
-    
-    def clean(self):
-        super().clean()
-        errors = {}
-        
-        if self.course.is_published and self.pk:
-            old_instance = FAQ.objects.get(pk=self.pk)
-            
-            if old_instance.is_deleted == False and self.is_deleted == True:
-                errors['is_deleted'] = _("امکان حذف سوال متداول برای دوره‌های منتشر شده وجود ندارد.")
-        
-        if errors:
-            raise ValidationError(errors)
-    
-    def save(self, *args, **kwargs):
-        self.clean()
-        super().save(*args, **kwargs)
     
     class Meta:
         ordering = ['order', 'created_at', 'id']
@@ -359,10 +371,6 @@ class Season(models.Model):
 
     def __str__(self):
         return f"{self.title} - {self.course}"
-            
-    def save(self, *args, **kwargs):
-        self.clean()
-        super().save(*args, **kwargs)
 
     class Meta:
         ordering = ['order', 'created_at', 'id']
@@ -374,7 +382,7 @@ class Lesson(models.Model):
     title = models.CharField(max_length=100, verbose_name=_('عنوان درس'))
     url_video = models.URLField(verbose_name=_('آدرس ویدیو'), unique=True)
     order = models.PositiveSmallIntegerField(blank=True, null=True, verbose_name=_('ترتیب دوره'))
-    url_files = models.URLField(blank=True, null=True, unique=True, verbose_name=_('آدرس فایل ها'))
+    url_attachment = models.URLField(blank=True, null=True, unique=True, verbose_name=_('آدرس فایل ها'))
     is_published = models.BooleanField(default=False, verbose_name=_('وضعیت انتشار'))
     is_deleted = models.BooleanField(default=False, verbose_name=_('وضعیت حذف'))
     season = models.ForeignKey(
@@ -402,27 +410,13 @@ class Lesson(models.Model):
         super().clean()
         errors = {}
 
-        if self.pk:
-            old_instance = Lesson.objects.get(pk=self.pk)
-
-            if self.course.is_published:
-                if not old_instance.is_deleted and self.is_deleted:
-                    errors['is_deleted'] = _("امکان حذف جلسه برای دوره‌های منتشر شده وجود ندارد.")
-
-                if old_instance.is_published and not self.is_published:
-                    errors['is_published'] = _("امکان تغییر وضعیت از انتشار به عدم انتشار جلسه برای دوره‌های منتشر شده وجود ندارد.")
-                
-            if hasattr(self, 'season') and self.season.course != self.course:
-                errors['season'] = _(
-                    "فصل انتخاب شده با دوره انتخاب شده یکسان نیست. "
-                )
-                errors['course'] = _(
-                    "دوره انتخاب شده با فصل انتخاب شده یکسان نیست. "
-                )
-            
-            else:
-                if self.is_published:
-                    errors['is_published'] = _("اگر دوره منتشر نشده باشد امکان انتشار برای جلسه هم ممکن نیست.")
+        if self.pk and self.season and self.season.course != self.course:
+            errors['season'] = _(
+                "فصل انتخاب شده با دوره انتخاب شده یکسان نیست. "
+            )
+            errors['course'] = _(
+                "دوره انتخاب شده با فصل انتخاب شده یکسان نیست. "
+            )
 
         if errors:
             raise ValidationError(errors)
@@ -440,3 +434,123 @@ class Lesson(models.Model):
         ordering = ['order', 'published_at', 'id']
         verbose_name = _('درس')
         verbose_name_plural = _('درس ها')
+
+
+class LessonMedia(models.Model):
+    course = models.ForeignKey(
+        Course,
+        on_delete=models.CASCADE,
+        related_name='media',
+    )
+    video = models.FileField(upload_to=get_upload_videos, blank=True, null=True)
+    attachment = models.FileField(upload_to=get_upload_attachments, blank=True, null=True)
+    uploaded_by = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    uploaded_at = models.DateTimeField(auto_now_add=True)
+    
+    def clean(self):
+        super().clean()
+        errors = {}
+        
+        if not self.video and not self.attachment:
+            error_msg = _("حداقل یکی از فیلدهای 'ویدیو' یا 'پیوست' باید مقدار داشته باشد.")
+            errors['video'] = error_msg
+            errors['attachment'] = error_msg
+        
+        if errors:
+            raise ValidationError(errors)
+        
+    def save(self, *args, **kwargs):
+        self.clean()
+        super().save(*args, **kwargs)
+    
+    def __str__(self):
+        return f"Media for {self.course}"
+    
+    class Meta:
+        verbose_name = _("رسانه‌ی درس")
+        verbose_name_plural = _("رسانه‌های درس")
+
+
+class CourseRequest(models.Model): 
+    target_type = models.CharField(max_length=20, choices=RequestTargetTypeChoices.choices)
+    target_id = models.PositiveIntegerField(null=True, blank=True)
+    action = models.CharField(max_length=20, choices=RequestActionChoices.choices)
+    status = models.CharField(
+        max_length=20, choices=RequestStatusChoices.choices,
+        default=RequestStatusChoices.DRAFT,
+    )
+    data = models.JSONField()
+    history = HistoricalRecords()
+    comments = models.TextField(null=True, blank=True)
+    admin_response = models.TextField(null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    is_deleted = models.BooleanField(default=False)
+    need_revision = models.BooleanField(default=False)
+    teacher = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name='course_requests'
+    )
+    admin = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.SET_NULL,
+        null=True, blank=True,
+        editable=False,
+        related_name='admin_course_requests'
+    )
+    
+    def clean(self):
+        super().clean()
+        errors = {}
+        
+        error_message = _("یافت نشد.")
+        
+        model_mapping = {
+            RequestTargetTypeChoices.COURSE: (Course, 'teacher'),
+            RequestTargetTypeChoices.SEASON: (Season, 'course__teacher'),
+            RequestTargetTypeChoices.LESSON: (Lesson, 'course__teacher'),
+            RequestTargetTypeChoices.FAQ: (FAQ, 'course__teacher'),
+            RequestTargetTypeChoices.FEATURE: (Feature, 'course__teacher'),
+        }
+        
+        if self.action != RequestActionChoices.ADD and self.target_type in model_mapping:
+            ModelClass, teacher_field = model_mapping[self.target_type]
+            filter_kwargs = {
+                'pk': self.target_id,
+                'is_deleted': False,
+            }
+            filter_kwargs[teacher_field] = self.teacher
+
+            if not ModelClass.objects.filter(**filter_kwargs).exists():
+                errors['target_id'] = error_message
+        
+        if self.status == RequestStatusChoices.NEED_REVISION and (not self.admin_response or self.admin_response == ''):
+            errors['admin_response'] = _("در وضعیت نیاز به اصلاح باید پاسخی به مدرس داده شود.")
+            
+        if self.action == RequestActionChoices.DELETE and (not self.comments or self.comments == ''):
+            errors['comments'] = _("برای درخواست از نوع حذف باید توضیحات درج شود.")
+            
+        if self.action != RequestActionChoices.ADD and not self.target_id:
+            errors['target_id'] = _("نباید خالی باشد.")
+
+        if errors:
+            raise ValidationError(errors)
+        
+    def save(self, *args, **kwargs):
+        self.clean()
+        
+        if self.status == RequestStatusChoices.NEED_REVISION:
+            self.need_revision = True
+        
+        super().save(*args, **kwargs)
+        
+    def __str__(self):
+        return f"CourseRequest(id={self.pk}, action={self.action}, status={self.status})"
+
+    class Meta:
+        verbose_name = _("درخواست")
+        verbose_name_plural = _("درخواست ها")
+        ordering = ['-created_at', '-id']
+
+# endregion
