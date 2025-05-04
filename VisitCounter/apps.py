48a7f29fd62d6f7cd51513f228477701ea819ac7
aliazizi-code <devaliazizi@gmail.com>
2025-05-04 14:57:59 +0330

    fix: correct app name casing and ensure signal imports in VisitCounterConfig

--- VisitCounter/apps.py
+++ VisitCounter/apps.py
@@ -3,4 +3,7 @@ from django.apps import AppConfig
 
 class VisitCounterConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'visit_counter'
+    name = 'VisitCounter'
+    
+    def ready(self):
+        import VisitCounter.signals
