c95ecb05ce6a961b3a105a7bb5d91c59526db335
aliazizi-code <devaliazizi@gmail.com>
2025-05-05 13:41:27 +0330

    feat: add validation in ContentVisit model to restrict allowed visit models

--- VisitCounter/models.py
+++ VisitCounter/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.contenttypes.models import ContentType
 from django.contrib.contenttypes.fields import GenericForeignKey
 from django.utils.translation import gettext_lazy as _
+from django.core.exceptions import ValidationError
 
 
 class ContentVisit(models.Model):
@@ -19,6 +20,17 @@ class ContentVisit(models.Model):
     
     def __str__(self):
         return f"بازدید {self.content_type} ({self.object_slug}) - {self.created_at.strftime('%Y-%m-%d %H:%M:%S')}"
+    
+    def clean(self):
+        super().clean()
+        
+        ALLOWED_VISIT_MODELS = ["article", "course"]
+        
+        if self.content_type and self.object_slug:
+            related_model = self.content_type.model_class()
+            
+            if related_model._meta.model_name.lower() not in ALLOWED_VISIT_MODELS:
+                raise ValidationError(_("این مدل اجازه‌ی دریافت کامنت را ندارد."))
     
     class Meta:
         verbose_name = _("بازدید محتوا")
