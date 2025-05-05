28fb18fe157f6c425e0712976fbac9d150512841
aliazizi-code <devaliazizi@gmail.com>
2025-05-05 13:23:08 +0330

    Refactor URL patterns in comments app for clarity; separate create, delete, and list routes with appropriate naming.

--- comments/urls.py
+++ comments/urls.py
@@ -1,9 +1,22 @@
-from django.urls import path
+from django.urls import re_path, path
 
 from . import views
 
 
 urlpatterns = [
-    path('', views.CommentViewSet.as_view({'get': 'list'}), name='comments'),
-    path('create/', views.CommentViewSet.as_view({'post': 'create'}), name='comments'),
+    path(
+        'create/',
+        views.CommentViewSet.as_view({'post': 'create'}),
+        name='comments-create'
+    ),
+    path(
+        'delete/<int:pk>/',
+        views.CommentViewSet.as_view({'delete': 'destroy'}),
+        name='comments-create'
+    ),
+    re_path(
+        r'^(?P<type>[\w\-]+)/(?P<slug>[\w\-\u0600-\u06FF]+)/?$',
+        views.CommentViewSet.as_view({'get': 'list'}),
+        name='comments-list'
+    ),
 ]
